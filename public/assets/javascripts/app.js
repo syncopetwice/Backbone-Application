c4204cfd8bf5aa35f7f40d229d56a4158eadb34c
Oleg Drozdov <oleg.drozdov.vrp@vrpinc.com>
2015-09-11 17:57:27 +0300

    Added Marionette Classes

--- public/assets/javascripts/app.js
+++ public/assets/javascripts/app.js
@@ -1,145 +1,112 @@
-var App, appCollectionsUser, userAvatar, userModel,
-  extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
+var extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
   hasProp = {}.hasOwnProperty;
 
-window.App = App || {};
+this.App = new Backbone.Marionette.Application;
 
-App = {
+this.App = {
   Models: {},
   Collections: {},
-  Views: {}
+  ItemViews: {},
+  CompositeViews: {}
 };
 
-App.Models.User = (function(superClass) {
-  extend(User, superClass);
+this.App.addRegions({
+  tableRegion: '#table-region'
+});
 
-  function User() {
-    return User.__super__.constructor.apply(this, arguments);
+this.App.on("initialize:after", function(options) {
+  if (Backbone.history) {
+    return Backbone.history.start();
+  }
+});
+
+
+/*
+Table
+ */
+
+App.Models.Table = (function(superClass) {
+  extend(Table, superClass);
+
+  function Table() {
+    return Table.__super__.constructor.apply(this, arguments);
   }
 
-  User.prototype.initialize = function() {};
+  Table.prototype.initialize = function() {
+    return console.log('M-Table init');
+  };
 
-  return User;
+  return Table;
 
 })(Backbone.Model);
 
-userModel = new App.Models.User({
-  user_name: 'BLBSR',
-  user_name_href: 'https://google.com',
-  user_image: 'http://orig01.deviantart.net/e483/f/2012/189/8/f/chibi_bulbasaur_by_o_melet-d56ijzg.png'
-});
+App.Collections.Table = (function(superClass) {
+  extend(Table, superClass);
 
-App.Collections.User = (function(superClass) {
-  extend(User, superClass);
-
-  function User() {
-    return User.__super__.constructor.apply(this, arguments);
+  function Table() {
+    return Table.__super__.constructor.apply(this, arguments);
   }
 
-  User.prototype.initialize = function() {};
+  Table.prototype.model = App.Models.Table;
 
-  User.prototype.model = App.Models.User;
+  Table.prototype.initialize = function() {
+    return console.log('C-Table init');
+  };
 
-  User.prototype.url = '/users';
-
-  return User;
+  return Table;
 
 })(Backbone.Collection);
 
-appCollectionsUser = new App.Collections.User;
+App.ItemViews.EmptyView = (function(superClass) {
+  extend(EmptyView, superClass);
 
-appCollectionsUser.fetch();
-
-
-/*
-	Parent
- */
-
-App.Views.User = (function(superClass) {
-  extend(User, superClass);
-
-  function User() {
-    return User.__super__.constructor.apply(this, arguments);
+  function EmptyView() {
+    return EmptyView.__super__.constructor.apply(this, arguments);
   }
 
-  User.prototype.model = appCollectionsUser;
+  EmptyView.prototype.template = "#empty-view";
 
-  User.prototype.initialize = function() {
-    return this.render();
+  EmptyView.prototype.initialize = function(options) {
+    return console.log(options);
   };
 
-  User.prototype.render = function() {
-    this.$el.html(this.template(this.model.toJSON()));
-    return this;
-  };
+  return EmptyView;
 
-  return User;
+})(Marionette.ItemView);
 
-})(Backbone.View);
+App.ItemViews.TableRow = (function(superClass) {
+  extend(TableRow, superClass);
 
-
-/*
-	Childs Extends Parent
- */
-
-
-/*
-	User Avatar View
- */
-
-App.Views.User.Avatar = (function(superClass) {
-  extend(Avatar, superClass);
-
-  function Avatar() {
-    return Avatar.__super__.constructor.apply(this, arguments);
+  function TableRow() {
+    return TableRow.__super__.constructor.apply(this, arguments);
   }
 
-  Avatar.prototype.template = _.template($('#avatar').html());
+  TableRow.prototype.template = "#table-row";
 
-  Avatar.prototype.el = $('.avatar');
+  return TableRow;
 
-  return Avatar;
+})(Marionette.ItemView);
 
-})(App.Views.User);
+App.CompositeViews.Table = (function(superClass) {
+  extend(Table, superClass);
+
+  function Table() {
+    return Table.__super__.constructor.apply(this, arguments);
+  }
+
+  Table.prototype.template = "#table";
+
+  Table.prototype.itemView = App.ItemViews.TableRow;
+
+  Table.prototype.itemViewContainer = 'tbody';
+
+  Table.prototype.emptyView = App.ItemViews.EmptyView;
+
+  return Table;
+
+})(Marionette.CompositeView);
 
 
 /*
-	User Data View
+Routers
  */
-
-App.Views.User.Data = (function(superClass) {
-  extend(Data, superClass);
-
-  function Data() {
-    return Data.__super__.constructor.apply(this, arguments);
-  }
-
-  Data.prototype.template = _.template($('#data').html());
-
-  Data.prototype.el = $('.data');
-
-  return Data;
-
-})(App.Views.User);
-
-
-/*
-	User Dates View
- */
-
-App.Views.User.Dates = (function(superClass) {
-  extend(Dates, superClass);
-
-  function Dates() {
-    return Dates.__super__.constructor.apply(this, arguments);
-  }
-
-  Dates.prototype.template = _.template($('#dates').html());
-
-  Dates.prototype.el = $('.dates');
-
-  return Dates;
-
-})(App.Views.User);
-
-userAvatar = new App.Views.User.Avatar;
--- public/coffee/app.coffee
+++ public/coffee/app.coffee
@@ -1,91 +1,45 @@
-window.App = App || {}
+@App = new Backbone.Marionette.Application
 
-App =
+@App = 
 	Models: {}
 	Collections: {}
-	Views: {}
+	ItemViews: {}
+	CompositeViews: {}
 
-class App.Models.User extends Backbone.Model
+@App.addRegions
+	tableRegion: '#table-region'
+
+@App.on "initialize:after", (options) ->
+	if Backbone.history
+		Backbone.history.start()
+
+
+###
+Table
+###
+class App.Models.Table extends Backbone.Model
 	initialize: ->
-		# console.log ':: App.Models.User'
+		console.log 'M-Table init'
 
-userModel = new App.Models.User
-	user_name: 'BLBSR'
-	user_name_href: 'https://google.com'
-	user_image: 'http://orig01.deviantart.net/e483/f/2012/189/8/f/chibi_bulbasaur_by_o_melet-d56ijzg.png'
-	# data_role: "Backbone Developer"
-	# data_annual_sales: "1,000,000"
-	# data_coach: "Michael Scott"
-	# data_predicted_sales: "1,500,000"
-	# new_opp_created: 9
-	# new_opp_expected: 12
-	# closed_won_wins: 7
-	# closed_won_expected: 9
-	# sales_vs_target_percentage: '25%'
-	# dates_sales: 300000
-	# dates_target: 450000
-
-
-
-class App.Collections.User extends Backbone.Collection
+class App.Collections.Table extends Backbone.Collection
+	model: App.Models.Table
 	initialize: ->
-		# console.log ':: App.Collections.User'
-	model: App.Models.User
-	url: '/users'
+		console.log 'C-Table init'
 
-appCollectionsUser = new App.Collections.User
-appCollectionsUser.fetch()
+class App.ItemViews.EmptyView extends Marionette.ItemView
+	template: "#empty-view"
+	initialize: (options) ->
+		console.log options
 
+class App.ItemViews.TableRow extends Marionette.ItemView
+	template: "#table-row"
 
-###
-	Parent
-###
-class App.Views.User extends Backbone.View
-
-	model: appCollectionsUser
-
-	initialize: ->
-		@render()
-
-
-	render: ->
-		@$el.html(@template(@model.toJSON()))
-		@
-
+class App.CompositeViews.Table extends Marionette.CompositeView
+	template: "#table"
+	itemView: App.ItemViews.TableRow
+	itemViewContainer: 'tbody'
+	emptyView: App.ItemViews.EmptyView
 
 ###
-	Childs Extends Parent
+Routers
 ###
-
-###
-	User Avatar View
-###
-class App.Views.User.Avatar extends App.Views.User
-
-	template: _.template($('#avatar').html())
-
-	el: $('.avatar')
-
-###
-	User Data View
-###
-class App.Views.User.Data extends App.Views.User
-
-	template: _.template($('#data').html())
-
-	el: $('.data')
-
-###
-	User Dates View
-###
-class App.Views.User.Dates extends App.Views.User
-
-	template: _.template($('#dates').html())
-
-	el: $('.dates')
-
-userAvatar = new App.Views.User.Avatar
-# userData = new App.Views.User.Data
-# userDates = new App.Views.User.Dates
-
-
